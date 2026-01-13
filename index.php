5607d4a2335ec70bcb66657eda9bb4ed806de84d
Wojtek <wojtek.lubawy@gmail.com>
2026-01-13 09:09:50 +0100

    pierwsze podstawy

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="Dheader">
+    <h1 style="text-align: center;">Skoki</h1>
+    </div>
+    <div id="divination">
+    <input type="text" id="search" placeholder="Szukaj...">
+    <input type="button" value="Szukaj" id="searchBtn">
+    </div>
+    
+    <div class="Dmenu">
+    </div>
+</body>
+</html>
