cb3dc2a9e50ddef42f368202c9722eed8f86aabb
Wojtek <wojtek.lubawy@gmail.com>
2026-01-14 09:06:58 +0100

    baza danych z drugiego dnia

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 13, 2026 at 09:16 AM
+-- Generation Time: Jan 14, 2026 at 09:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `firmy`
+--
+
+CREATE TABLE `firmy` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(100) NOT NULL,
+  `ulica` varchar(100) NOT NULL,
+  `kod_pocztowy` varchar(6) NOT NULL,
+  `NIP` int(9) NOT NULL,
+  `REGON` int(14) NOT NULL,
+  `nr_telefonu` varchar(12) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategorie`
+--
+
+CREATE TABLE `kategorie` (
+  `id` int(11) NOT NULL,
+  `nazwa` int(100) NOT NULL,
+  `id_nad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentarze`
+--
+
+CREATE TABLE `komentarze` (
+  `id` int(11) NOT NULL,
+  `tresc` varchar(511) NOT NULL,
+  `autor` varchar(100) NOT NULL,
+  `data_utworzenia` datetime NOT NULL,
+  `data_mod` datetime NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -39,7 +83,28 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `pass`, `email`) VALUES
-(0, 'admin', 'admin', 'admin');
+(0, 'admin', 'admin', 'admin'),
+(0, 'Jan', 'haslo', 'jan@kowalski.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `firmy`
+--
+ALTER TABLE `firmy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `firmy`
+--
+ALTER TABLE `firmy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
